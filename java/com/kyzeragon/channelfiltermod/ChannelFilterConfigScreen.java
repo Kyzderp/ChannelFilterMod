bb04aa21ce70ae9641c4bb386cb950bb0f6e1ce5
Kyzderp <hannah_chu@yahoo.com>
2015-02-25 05:14:25 -0800

    Woots. Implemented everything I wanted to.

--- java/com/kyzeragon/channelfiltermod/ChannelFilterConfigScreen.java
+++ java/com/kyzeragon/channelfiltermod/ChannelFilterConfigScreen.java
@@ -1,57 +1,134 @@
 package com.kyzeragon.channelfiltermod;
 
+import java.util.LinkedList;
+
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 
 public class ChannelFilterConfigScreen extends GuiScreen 
 {
-	private String[] channels = {"Global", "Help", "Trade", "Local", "Faction", "Ally"};
+	// TODO: display staff, display when name is said
+	private String[] channels = {"Global", "Help", "Trade", "Local", "Faction", "Allies"};
 	private boolean[] isChannelShown;
-	
+	private boolean showStaff;
+	private boolean showSelf;
+	private LinkedList<Character> ch = new LinkedList<Character>();
+	private int cooldown; // cooldown for clicking cuz buttons are dumb
+
+
 	public ChannelFilterConfigScreen()
 	{
 		super();
 		this.isChannelShown = new boolean[channels.length];
-		for (int i = 0; i < channels.length; i++)
-			this.isChannelShown[i] = true;
+		char[] letters = {'G', 'H', 'T', 'L', 'F', 'A'};
 		for (int i = 0; i < channels.length; i++)
 		{
-			this.buttonList.add(new GuiButton(i, this.width/2 - 100, this.height/2 - 75 + 25*i, 
-					200, 20, channels[i] + ": Shown"));
+			this.isChannelShown[i] = true;
+			this.ch.addLast(letters[i]);
 		}
+		this.showStaff = true;
+		this.showSelf = true;
 	}
-	
+
 	@Override
 	public void initGui()
 	{
 		super.initGui();
 		this.drawDefaultBackground();
+		for (int i = 0; i < channels.length; i++)
+		{
+			if (i < 3)
+				this.buttonList.add(new GuiButton(i, this.width/2 - 152, 
+						this.height/2 - 50 + 25*i, 150, 20, ""));
+			else
+				this.buttonList.add(new GuiButton(i, this.width/2 + 2, 
+						this.height/2 - 125 + 25*i, 150, 20, ""));
+		}
+		this.buttonList.add(new GuiButton(6, this.width/2 - 152, this.height/2 + 55, 150, 20, ""));
+		this.buttonList.add(new GuiButton(7, this.width/2 + 2, this.height/2 + 55, 150, 20, ""));
 	}
-	
+
 	@Override
 	public void drawScreen(int par1, int par2, float par3) //no idea what the params are for :D
 	{
+		if (this.cooldown > 0)
+			this.cooldown--;
 		this.drawDefaultBackground();
-		this.drawCenteredString(fontRendererObj, "Channel Filter Options", 
-				this.width/2, this.height/2 - 100, 0xFFFFFF);
 		super.drawScreen(par1, par2, par3);
+		
+		this.drawCenteredString(fontRendererObj, "Channel Filter Options", 
+				this.width/2, this.height/2 - 80, 0xFFFFFF);
+		this.drawCenteredString(fontRendererObj, "Channels",
+				this.width/2, this.height/2 - 65, 0xFFFFFF);
+		this.drawCenteredString(fontRendererObj, "Other Options",
+				this.width/2, this.height/2 + 40, 0xFFFFFF);
+
+		String display;
+		int color = 0x88FFAA;
+		GuiButton button;
+		for (int i = 0; i < channels.length; i++)
+		{
+			display = channels[i] + ": Shown";
+			color = 0x88FFAA;
+			if (!this.isChannelShown[i])
+			{
+				color = 0xFF8888;
+				display = channels[i] + ": Hidden";
+			}
+			if (i < 3)
+				this.drawCenteredString(fontRendererObj, display, 
+						this.width/2 - 152 + 75, this.height/2 - 50 + 25*i + 6, color);
+			else
+				this.drawCenteredString(fontRendererObj, display, 
+						this.width/2 + 2 + 75, this.height/2 - 125 + 25*i + 6, color);
+		}
+		color = 0x88FFAA;
+		display = "Always Show Staff: True";
+		if (!this.showStaff)
+		{
+			color = 0xFF8888;
+			display = "Always Show Staff: False";
+		}
+		this.drawCenteredString(fontRendererObj, display, 
+				this.width/2 - 152 + 75, this.height/2 + 55 + 6, color);
+
+		color = 0x88FFAA;
+		display = "Show Own Name: True";
+		if (!this.showSelf)
+		{
+			color = 0xFF8888;
+			display = "Show Own Name: False";
+		}
+		this.drawCenteredString(fontRendererObj, display, 
+				this.width/2 + 2 + 75, this.height/2 + 55 + 6, color);
 	}
-	
+
 	@Override
 	protected void actionPerformed(GuiButton button)
 	{
 		int i = button.id;
-		this.isChannelShown[i] = !this.isChannelShown[i];
-		if (this.isChannelShown[i])
-			button.displayString = this.channels[i] + ": Shown";
-		else
-			button.displayString = this.channels[i] + ": OFF";
+		if (i < 6) // channel buttons
+			this.isChannelShown[i] = !this.isChannelShown[i];
+		else if (i == 6)
+			this.showStaff = !this.showStaff;
+		else if (i == 7)
+			this.showSelf = !this.showSelf;
 		this.updateScreen();
 	}
-	
+
 	@Override
 	public void updateScreen()
 	{
 		super.updateScreen();
 	}
+
+	public boolean getShown(char c)
+	{
+		int i = ch.indexOf(c);
+		return this.getShown(i);
+	}
+
+	public boolean getShown(int i) { return this.isChannelShown[i];	}
+	public boolean getStaff() { return this.showStaff; }
+	public boolean getSelf() { return this.showSelf; }
 }
--- java/com/kyzeragon/channelfiltermod/LiteModChannelFilter.java
+++ java/com/kyzeragon/channelfiltermod/LiteModChannelFilter.java
@@ -8,6 +8,9 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.network.play.client.C01PacketChatMessage;
 import net.minecraft.network.play.server.S02PacketChat;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
 
 import com.mumfrey.liteloader.ChatFilter;
@@ -20,12 +23,11 @@ public class LiteModChannelFilter implements ChatFilter, Tickable
 	private ChannelFilterConfigScreen configScreen;
 	private static KeyBinding configKeyBinding;
 
+	@Override
+	public String getName() { return "TE Channel Filter"; }
 
 	@Override
-	public String getName() { return "Channel Filter"; }
-
-	@Override
-	public String getVersion() { return "0.9.0"; }
+	public String getVersion() { return "1.0.0"; }
 
 	@Override
 	public void init(File configPath) 
@@ -38,26 +40,54 @@ public class LiteModChannelFilter implements ChatFilter, Tickable
 	@Override
 	public void upgradeSettings(String version, File configPath, File oldConfigPath) {}
 
+	/**
+	 * Filters out chat messages from the specified channels and with options
+	 */
 	@Override
 	public boolean onChat(S02PacketChat chatPacket, IChatComponent chat, String message) 
-	{
-		// TODO Auto-generated method stub
-		// TODO: error if talking in hidden channel
+	{ // "Global", "Help", "Trade", "Local", "Faction", "Ally"
+		String playerName = Minecraft.getMinecraft().thePlayer.getCommandSenderName();
+		// TODO: shop? lobaloba?
+		if (message.matches("§r§8\\[§r§f.§r§8\\]§r§7=.*"))
+		{
+			char ch = message.charAt(9);
+			if (this.configScreen.getShown(ch))
+				return true;
+			else
+			{
+				if (message.matches(".*§a" + playerName + ".*")) // talking in a hidden channel
+				{
+					this.logError("[!] You are talking in a hidden channel: [" + ch + "]");
+					return true;
+				}
+				else if (this.configScreen.getStaff() && message.matches(".*§8\\[§r§(6Admin|6Dev|eMod|bAssistant)§r§8\\].*"))
+					return true;
+				else if (this.configScreen.getSelf()  // someone else mentions your name
+						&& message.toLowerCase().matches(".*"+playerName.toLowerCase()+".*"))
+					return true;
+				return false;
+			}
+		}
 		return true;
 	}
 
 	@Override
 	public void onTick(Minecraft minecraft, float partialTicks, boolean inGame, boolean clock) 
 	{
-		if (inGame && minecraft.currentScreen != null && minecraft.currentScreen.equals(this.configScreen))
-		{
-//			this.configScreen.updateScreen();
-		}
-		
 		if (inGame && minecraft.currentScreen == null && this.configKeyBinding.isPressed())
 		{
 			minecraft.displayGuiScreen(this.configScreen);
 		}
 	}
 
+	/**
+	 * Logs error message in red text to the user
+	 * @param message The error message
+	 */
+	private void logError(String message)
+	{
+		ChatComponentText displayMessage = new ChatComponentText(message);
+		displayMessage.setChatStyle((new ChatStyle()).setColor(EnumChatFormatting.RED));
+		Minecraft.getMinecraft().thePlayer.addChatComponentMessage(displayMessage);
+	}
 }
